92737c8e32efdd025389cc7ac16cd9e046d71969
DzEN <dandyoctavian@gmail.com>
2025-05-14 15:07:15 +0700

    Add Footer component to main Layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react"
 import { Header } from "./Header"
+import { Footer } from "./Footer"
 
 interface LayoutProps {
   children: ReactNode
@@ -10,6 +11,7 @@ export function Layout({ children }: LayoutProps) {
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-grow pt-16">{children}</main>
+      <Footer />
     </div>
   )
 } 
